c20f718eac7223a630cfe1816ed4e111785997f3
JohnnyOhms <officialjohn662@gmail.com>
2023-02-24 14:52:27 +0100

    async handler middleware

---
+++ middlewares/asyncHandler.js
@@ -0,0 +1,11 @@
+const asyncHandler = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+module.exports = asyncHandler;
