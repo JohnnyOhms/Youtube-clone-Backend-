ae2a054194fc0321511fd4312d1d14fcd8438741
JohnnyOhms <officialjohn662@gmail.com>
2023-02-26 23:59:05 +0100

    route updated

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const Router = express.Router();
-const { register, login } = require("../controllers/auth");
+const {
+  register,
+  login,
+  forgetPassword,
+  resetPassword,
+  confirmResetPassword,
+} = require("../controllers/auth");
 
 Router.route("/register").post(register);
 Router.route("/login").post(login);
+Router.route("/forgetPassword").post(forgetPassword);
+Router.route("/resetPassword/:_id/:token").get(resetPassword);
+Router.route("/confirmResetPassword").post(resetPassword);
 
 module.exports = Router;
